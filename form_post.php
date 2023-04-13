221ef13aecd53132430305ab8e465bf757006756
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-04-13 12:22:37 +0900

    画像ファイルアップロード作成

--- form_post.php
+++ form_post.php
@@ -34,12 +34,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>アンケート結果</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-        <h1>アンケート結果</h1>
-        <p>お名前：<?php echo $name; ?> </p>
-        <p>性別：<?php echo $gender; ?> </p>
-        <p>評価：<?php echo $star; ?> </p>
-        <p>ご意見：<?php echo $other; ?> </p>
+    <?php include('navbar.php'); ?>
+    <main role="main" class="container">
+    <h1>アンケート結果</h1>
+    <p>お名前：<?php echo $name; ?> </p>
+    <p>性別：<?php echo $gender; ?> </p>
+    <p>評価：<?php echo $star; ?> </p>
+    <p>ご意見：<?php echo $other; ?> </p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
